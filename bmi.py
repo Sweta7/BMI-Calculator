d63c109beee1792f90c12683bba25324cc6f2931
Sweta7 <anvita1982@gmail.com>
2018-04-11 14:04:43 +1200

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,9 +1,9 @@
+# BMI calculator code on python.
 name = input('Enter your name: ')
 weight = int(input("Enter your weight (kg): "))
-#print('Weight', weight)
 height = int(input("Enter you height (cm): "))
 bmi = round(weight/(height/100) **2,2)
-#output = round(x,2)
+# output = round(x,2) "to wound the decimal value"
 if (bmi>=25 and bmi<=24.99):
           print(name, "your BMI is ",bmi, "and you are overweight")
 elif (bmi<18.5):
