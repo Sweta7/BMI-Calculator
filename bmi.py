28a8b9305d8c90aa1c749b4ff16c6c80ac4a89e8
Sweta7 <anvita1982@gmail.com>
2018-04-11 14:02:37 +1200

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,14 @@
+name = input('Enter your name: ')
+weight = int(input("Enter your weight (kg): "))
+#print('Weight', weight)
+height = int(input("Enter you height (cm): "))
+bmi = round(weight/(height/100) **2,2)
+#output = round(x,2)
+if (bmi>=25 and bmi<=24.99):
+          print(name, "your BMI is ",bmi, "and you are overweight")
+elif (bmi<18.5):
+          print(name, "your BMI is ",bmi, "and your are underweight")
+elif (bmi>=18.5 and bmi<=24.99):
+          print(name, "your BMI is ",bmi, "and your are normal weight")
+else:
+          print(name, "your BMI is ",bmi, "and your are obese")
